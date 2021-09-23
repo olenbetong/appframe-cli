71902e7300036be88a2b67cd3eadd050c8f5085a
Bjørnar Vister Hansen <bvh@olenbetong.no>
2021-09-23 16:02:55 +0200

    Move more stuff to the Server class

--- cli/af-prepare-bundle.js
+++ cli/af-prepare-bundle.js
@@ -1,15 +1,12 @@
+import chalk from "chalk";
 import { config } from "dotenv";
-import { fileFromPath } from "formdata-node/file-from-path";
-
-import { FileUploader, login, setHostname } from "@olenbetong/data-object/node";
 
 import dsBundles from "../data/dsBundles.js";
+import { Server } from "../lib/Server.js";
 import { execShellCommand } from "../lib/execShellCommand.js";
 import { importJson } from "../lib/importJson.js";
 
 config({ path: process.cwd() + "/.env" });
-
-let { APPFRAME_LOGIN: username, APPFRAME_PWD: password } = process.env;
 
 dsBundles.errorHandler = (error) => {
   throw Error(error);
@@ -22,24 +19,17 @@ try {
   let tarball = `${safePackageName}-${packageConfig.version}.tgz`;
   let zipFileName = `${safePackageName}-${packageConfig.version}.zip`;
   let hostname = "dev.obet.no";
+  let server = new Server(hostname);
+  await server.login();
 
-  console.log("Logging in...");
-  setHostname(hostname);
-  await login(username, password);
-
-  console.log("Get bundle ID...");
-  dsBundles.setParameter(
-    "whereClause",
-    `[Name] = '${packageConfig.name}' AND [Version] = 0`
-  );
-  await dsBundles.refreshDataSource();
-
-  let { ID: id, Project_ID } =
-    dsBundles.getDataLength() > 0 ? dsBundles.getData(0) : {};
+  let bundle = await server.getBundle(packageConfig.name);
+  let { ID: id, Project_ID } = bundle;
   if (id) {
-    console.log(`Found bundle with Version/Project ID: ${id}/${Project_ID}`);
+    server.logServerMessage(
+      `Found bundle with Version/Project ID: ${id}/${Project_ID}`
+    );
   } else {
-    console.log("No bundle found...");
+    console.log(chalk.yellow("No bundle found..."));
   }
 
   console.log("Removing previous...");
@@ -61,15 +51,7 @@ try {
   await execShellCommand(`rm ${tarball}`);
 
   if (id) {
-    console.log("Uploading zip...");
-    let uploader = new FileUploader({
-      route: `/api/bundle-push/${id}`,
-      hostname,
-    });
-    let zipFile = await fileFromPath(
-      `${safePackageName}-${packageConfig.version}.zip`
-    );
-    await uploader.upload(zipFile, { Project_ID });
+    await server.uploadBundle(Project_ID, id, zipFileName);
     await execShellCommand(`rm ${zipFileName}`);
     console.log("Done!");
   } else {
--- cli/af-publish-to-prod.js
+++ cli/af-publish-to-prod.js
@@ -1,164 +1,53 @@
-import { config } from "dotenv";
-import * as af from "@olenbetong/data-object/node";
 import { importJson } from "../lib/importJson.js";
-import {
-  dsArticles,
-  dsArticlesVersions,
-  dsTransactions,
-  procCheckoutArticle,
-  procApply,
-  procDeploy,
-  procGenerate,
-  procPublishArticle,
-} from "../data/index.js";
-import { checkOnlyOneTransaction } from "../lib/checkOnlyOneTransaction.js";
 
-config({ path: process.cwd() + "/.env" });
+import { Server } from "../lib/Server.js";
 
 const pkg = await importJson("./package.json", true);
-let { APPFRAME_LOGIN: username, APPFRAME_PWD: password } = process.env;
 
 async function runStageOperations(hostname, operations = ["download"]) {
   let lastSuccessfulStep = "none";
   try {
-    af.setHostname(hostname);
-    console.log(`Logging in (${hostname}, user '${username}')...`);
-    await af.login(username, password);
+    let server = new Server(hostname);
+    await server.login();
     lastSuccessfulStep = "login";
 
-    console.log("Getting article information...");
-    dsArticles.setParameter(
-      "whereClause",
-      `[HostName] = '${pkg.appframe.hostname}' AND [ArticleId] = '${pkg.appframe.article}  '`
+    let transactionName = `${pkg.appframe.hostname}/${pkg.appframe.article}`;
+    let article = await server.getArticle(
+      pkg.appframe.hostname,
+      pkg.appframe.article
     );
-    await dsArticles.refreshDataSource();
     lastSuccessfulStep = "getArticleInformation";
 
-    let transactionName = `${pkg.appframe.hostname}/${pkg.appframe.article}`;
-    let article = dsArticles.getData(0);
-
     if (operations.includes("publish")) {
-      console.log("Checking out article...");
-      await procCheckoutArticle.executeAsync({
-        HostName: pkg.appframe.hostname,
-        ArticleID: pkg.appframe.article,
-        Forced: true,
-      });
-
-      lastSuccessfulStep = "checkout";
-
-      dsArticlesVersions.setParameter(
-        "whereClause",
-        `[HostName] = '${pkg.appframe.hostname}' AND [ArticleId] LIKE '${pkg.appframe.article}.___'`
-      );
-      await dsArticlesVersions.refreshDataSource();
-
-      dsTransactions.setParameter(
-        "whereClause",
-        `[Name] = '${pkg.appframe.hostname}/${pkg.appframe.article}'`
-      );
-      dsTransactions.setParameter("sortOrder", [{ Version: "desc" }]);
-      await dsTransactions.refreshDataSource();
-
-      // When we restore dev server, we sometimes get a mismatch between article versions
-      // and transaction versions. To be able to generate a transaction, we have to publish
-      // the app until the latest article version is higher than the latest transaction
-      // version.
-      if (
-        dsTransactions.getDataLength() > 0 &&
-        dsArticlesVersions.getDataLength() > 0
-      ) {
-        let transactionVersion = dsTransactions.getData(0, "Version");
-        let articleVersion = Number(
-          dsArticlesVersions.getData(0, "ArticleId").split(".")[1]
-        );
-
-        console.log(`Last published version: ${Number(articleVersion)}`);
-        console.log(`Last transaction version: ${Number(transactionVersion)}`);
-
-        let publishCount = 0;
-        while (articleVersion <= transactionVersion) {
-          console.log(`Publishing app (v${pkg.version})...`);
-
-          await procPublishArticle.execute({
-            FromHostName: pkg.appframe.hostname,
-            FromArticle: pkg.appframe.article,
-            ToArticle: pkg.appframe.article,
-            Description: "v" + pkg.version,
-          });
-
-          await dsArticlesVersions.refreshDataSource();
-          articleVersion = Number(
-            dsArticlesVersions.getData(0, "ArticleId").split(".")[1]
-          );
-
-          lastSuccessfulStep = `publish(${publishCount})`;
-        }
-      } else {
-        console.log(`Publishing app (v${pkg.version})...`);
-
-        await procPublishArticle.execute({
-          FromHostName: pkg.appframe.hostname,
-          FromArticle: pkg.appframe.article,
-          ToArticle: pkg.appframe.article,
-          Description: "v" + pkg.version,
-        });
-
-        lastSuccessfulStep = "publish";
-      }
+      await server.publishArticle(pkg.appframe.hostname, pkg.appframe.article);
+      lastSuccessfulStep = "publish";
     }
 
     if (operations.includes("download")) {
-      console.log("Downloading updates...");
-
-      let result = await af.afFetch(
-        `/api/ob-deploy/download/1/${article.Namespace}`,
-        {
-          method: "POST",
-        }
-      );
-
-      if (result.headers.get("Content-Type")?.includes("json")) {
-        let data = await result.json();
-
-        if (data?.error) {
-          throw Error(data.error);
-        }
-      }
-
-      if (!result.ok) {
-        throw Error(`${result.status} ${result.statusTest}`);
-      }
-
+      await server.download(article.Namespace);
       lastSuccessfulStep = "download";
     }
 
     if (operations.includes("generate")) {
-      console.log("Generating transactions...");
-      await procGenerate.execute({ Namespace_ID: article.Namespace_ID });
-
+      await server.generate(article.Namespace_ID);
       lastSuccessfulStep = "generate";
     }
 
     if (operations.includes("apply")) {
-      await checkOnlyOneTransaction(
+      await server.assertOnlyOneTransaction(
         `[Namespace_ID] = ${article.Namespace_ID} AND [Status] IN (0, 2, 4) AND [IsLocal] = 0`,
         transactionName
       );
-      console.log("Applying update...");
-      await procApply.execute({ Namespaces: String(article.Namespace_ID) });
-
+      await server.apply(article.Namespace_ID);
       lastSuccessfulStep = "apply";
     }
 
     if (operations.includes("deploy")) {
-      await checkOnlyOneTransaction(
+      await server.assertOnlyOneTransaction(
         `[Namespace_ID] = ${article.Namespace_ID} AND (([Status] = 0 AND [IsLocal] = 1) OR [Status] = 1)`,
         transactionName
       );
-      console.log("Deploying...");
-      await procDeploy.execute({ Namespace_ID: article.Namespace_ID });
-
+      await server.deploy(article.Namespace_ID);
       lastSuccessfulStep = "deploy";
     }
   } catch (error) {
