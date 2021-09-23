262b6426cd8cc0f578ab448a8ca4ed3250d12172
Bjørnar Vister Hansen <bvh@olenbetong.no>
2021-09-23 13:06:41 +0200

    Remove debug stuff

--- cli/af-update.js
+++ cli/af-update.js
@@ -4,15 +4,13 @@ import { importJson } from "../lib/importJson.js";
 try {
   let cliPkg = await importJson("../package.json");
   let cmd = "npm install -g @olenbetong/appframe-cli@latest";
-  console.log("sup?");
+
   try {
     let pkg = await importJson("./package.json", true);
     if (pkg.dependencies[cliPkg.name] || pkg.devDependencies[cliPkg.name]) {
       cmd = "npm install @olenbetong/appframe-cli@latest";
     }
-  } catch (error) {
-    console.log("No package.json found in CWD");
-  } // eslint-disable-line
+  } catch (error) {} // eslint-disable-line
 
   console.log(cmd);
   await execShellCommand(cmd);
