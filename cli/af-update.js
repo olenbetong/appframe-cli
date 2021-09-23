47f7c1ee3899a8eb914a119996461c33aae97110
Bjørnar Vister Hansen <bvh@olenbetong.no>
2021-09-23 13:05:53 +0200

    Check if we should update local instead of global

--- cli/af-update.js
+++ cli/af-update.js
@@ -1,9 +1,22 @@
 import { execShellCommand } from "../lib/execShellCommand.js";
+import { importJson } from "../lib/importJson.js";
 
 try {
+  let cliPkg = await importJson("../package.json");
   let cmd = "npm install -g @olenbetong/appframe-cli@latest";
+  console.log("sup?");
+  try {
+    let pkg = await importJson("./package.json", true);
+    if (pkg.dependencies[cliPkg.name] || pkg.devDependencies[cliPkg.name]) {
+      cmd = "npm install @olenbetong/appframe-cli@latest";
+    }
+  } catch (error) {
+    console.log("No package.json found in CWD");
+  } // eslint-disable-line
+
   console.log(cmd);
   await execShellCommand(cmd);
+
   console.log("Update completed.");
 } catch (error) {
   console.error("Update failed:", error.message);
