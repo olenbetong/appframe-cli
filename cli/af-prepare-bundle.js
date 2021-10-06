c31f80d3fd2afe1f2ec06d0b23c4b43f21f0e026
Bjørnar Vister Hansen <bvh@olenbetong.no>
2021-10-06 12:43:36 +0200

    Support preparing 3rd party bundles

--- cli/af-prepare-bundle.js
+++ cli/af-prepare-bundle.js
@@ -5,6 +5,7 @@ import dsBundles from "../data/dsBundles.js";
 import { Server } from "../lib/Server.js";
 import { execShellCommand } from "../lib/execShellCommand.js";
 import { importJson } from "../lib/importJson.js";
+import { Command } from "commander";
 
 config({ path: process.cwd() + "/.env" });
 
@@ -12,55 +13,79 @@ dsBundles.errorHandler = (error) => {
   throw Error(error);
 };
 
-try {
-  let packageConfig = await importJson("./package.json", true);
-  let safePackageName = packageConfig.name.replace("@", "").replace("/", "-");
-  let rootOutputFolder = packageConfig.name.split("/")[0];
-  let tarball = `${safePackageName}-${packageConfig.version}.tgz`;
-  let zipFileName = `${safePackageName}-${packageConfig.version}.zip`;
-  let hostname = "dev.obet.no";
-  let server = new Server(hostname);
-  await server.login();
+async function prepareBundle(pkgName) {
+  try {
+    let name;
+    let version;
 
-  let bundle = await server.getBundle(packageConfig.name);
-  let { ID: id, Project_ID } = bundle;
-  if (id) {
-    server.logServerMessage(
-      `Found bundle with Version/Project ID: ${id}/${Project_ID}`
-    );
-  } else {
-    console.log(chalk.yellow("No bundle found..."));
+    if (pkgName) {
+      name = pkgName;
+      version = await execShellCommand(`npm view ${pkgName} version`);
+      version = version.trim();
+    } else {
+      let packageConfig = await importJson("./package.json", true);
+      name = packageConfig.name;
+      version = packageConfig.version;
+    }
+
+    let safePackageName = name.replace("@", "").replace("/", "-");
+    let rootOutputFolder = name.split("/")[0];
+    let tarball = `${safePackageName}-${version}.tgz`;
+    let zipFileName = `${safePackageName}-${version}.zip`;
+    let hostname = "dev.obet.no";
+    let server = new Server(hostname);
+    await server.login();
+
+    let bundle = await server.getBundle(name);
+    let { ID: id, Project_ID } = bundle;
+    if (id) {
+      server.logServerMessage(
+        `Found bundle with Version/Project ID: ${id}/${Project_ID}`
+      );
+    } else {
+      console.log(chalk.yellow("No bundle found..."));
+    }
+
+    console.log("Removing previous...");
+    await execShellCommand(`rm -rf ${rootOutputFolder}`);
+    console.log("Packing...");
+    await execShellCommand(pkgName ? `npm pack ${pkgName}` : "npm pack");
+    console.log("Making output directory...");
+    await execShellCommand(`mkdir -p ${name}`);
+    console.log(`Extracting '${tarball}'...`);
+    await execShellCommand(`tar -xvzf ${tarball} -C ${name}`);
+    console.log(`Moving from 'package' to '${version}'...`);
+    await execShellCommand(`mv ${name}/package ${name}/${version}`);
+    console.log("Zipping...");
+    await execShellCommand(`zip -r ${zipFileName} ./${rootOutputFolder}`);
+    console.log("Cleaning...");
+    await execShellCommand(`rm -rf ${rootOutputFolder}`);
+    await execShellCommand(`rm ${tarball}`);
+
+    if (id) {
+      await server.uploadBundle(Project_ID, id, zipFileName);
+      await execShellCommand(`rm ${zipFileName}`);
+      console.log("Done!");
+    } else {
+      console.log("Launching explorer...");
+      try {
+        await execShellCommand("explorer.exe .");
+      } catch (error) {} // eslint-disable-line
+    }
+  } catch (error) {
+    console.log(chalk.red(error.message));
+    console.log(error.inner);
   }
-
-  console.log("Removing previous...");
-  await execShellCommand(`rm -rf ${rootOutputFolder}`);
-  console.log("Packing...");
-  await execShellCommand("npm pack");
-  console.log("Making output directory...");
-  await execShellCommand(`mkdir -p ${packageConfig.name}`);
-  console.log(`Extracting '${tarball}'...`);
-  await execShellCommand(`tar -xvzf ${tarball} -C ${packageConfig.name}`);
-  console.log(`Moving from 'package' to '${packageConfig.version}'...`);
-  await execShellCommand(
-    `mv ${packageConfig.name}/package ${packageConfig.name}/${packageConfig.version}`
-  );
-  console.log("Zipping...");
-  await execShellCommand(`zip -r ${zipFileName} ./${rootOutputFolder}`);
-  console.log("Cleaning...");
-  await execShellCommand(`rm -rf ${rootOutputFolder}`);
-  await execShellCommand(`rm ${tarball}`);
-
-  if (id) {
-    await server.uploadBundle(Project_ID, id, zipFileName);
-    await execShellCommand(`rm ${zipFileName}`);
-    console.log("Done!");
-  } else {
-    console.log("Launching explorer...");
-    try {
-      await execShellCommand("explorer.exe .");
-    } catch (error) {} // eslint-disable-line
-  }
-} catch (error) {
-  console.log(error.message);
-  console.log(error.inner);
 }
+
+const cliPkg = await importJson("../package.json");
+const program = new Command();
+program
+  .version(cliPkg.version)
+  .argument(
+    "[package]",
+    "Optional package name. If provided packs the npm package with name [package]"
+  )
+  .action(prepareBundle);
+
+await program.parseAsync();
