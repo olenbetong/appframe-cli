2a44ff94678c8dc7402a6b045c0033f3345b7b47
Bjørnar Vister Hansen <bvh@olenbetong.no>
2021-10-06 12:54:10 +0200

    Support providing package name as argument

--- cli/af-bundle-publish.js
+++ cli/af-bundle-publish.js
@@ -1,22 +1,28 @@
+import chalk from "chalk";
+import { Command } from "commander";
+
 import { Server } from "../lib/Server.js";
 import { importJson } from "../lib/importJson.js";
 
-const pkg = await importJson("./package.json", true);
-
-async function runStageOperations(hostname, operations = ["download"]) {
+async function runStageOperations(
+  packageName,
+  version,
+  hostname,
+  operations = ["download"]
+) {
   let lastSuccessfulStep = "none";
   try {
     let server = new Server(hostname);
     await server.login();
     lastSuccessfulStep = "login";
 
-    let bundle = await server.getBundle(pkg.name);
+    let bundle = await server.getBundle(packageName);
     if (!bundle || !bundle.ID)
-      throw Error(`Bundle with name '${pkg.name}' not found.`);
+      throw Error(`Bundle with name '${packageName}' not found.`);
 
     let { ID: id, Project_ID, Name, Namespace_ID } = bundle;
     if (!Namespace_ID) {
-      throw Error(`Bundle '${pkg.name}' is not assigned to a namespace`);
+      throw Error(`Bundle '${packageName}' is not assigned to a namespace`);
     }
 
     let namespace = await server.getNamespace(Namespace_ID);
@@ -26,7 +32,7 @@ async function runStageOperations(hostname, operations = ["download"]) {
     lastSuccessfulStep = "getBundleInformation";
 
     if (operations.includes("publish")) {
-      await server.publishBundle(Project_ID, id, `${pkg.name}@${pkg.version}`);
+      await server.publishBundle(Project_ID, id, `${packageName}@${version}`);
       lastSuccessfulStep = "publish";
     }
 
@@ -43,7 +49,7 @@ async function runStageOperations(hostname, operations = ["download"]) {
     if (operations.includes("apply")) {
       await server.assertOnlyOneTransaction(
         `[Namespace_ID] = ${Namespace_ID} AND [Status] IN (0, 2, 4) AND [IsLocal] = 0`,
-        pkg.name
+        packageName
       );
       await server.apply(namespace.ID);
       lastSuccessfulStep = "apply";
@@ -52,7 +58,7 @@ async function runStageOperations(hostname, operations = ["download"]) {
     if (operations.includes("deploy")) {
       await server.assertOnlyOneTransaction(
         `[Namespace_ID] = ${Namespace_ID} AND (([Status] = 0 AND [IsLocal] = 1) OR [Status] = 1)`,
-        pkg.name
+        packageName
       );
       await server.deploy(namespace.ID);
       lastSuccessfulStep = "deploy";
@@ -63,17 +69,48 @@ async function runStageOperations(hostname, operations = ["download"]) {
   }
 }
 
-async function publishFromDev() {
-  await runStageOperations("dev.obet.no", ["publish", "generate", "deploy"]);
-  await runStageOperations("stage.obet.no", ["download", "apply", "deploy"]);
-  await runStageOperations("test.obet.no", ["download"]);
+async function publishFromDev(packageName) {
+  try {
+    let name;
+    let version;
+    if (!packageName) {
+      const pkg = await importJson("./package.json", true);
+      name = pkg.name;
+      version = pkg.version;
+    } else {
+      name = packageName;
+      version = "latest";
+    }
+
+    await runStageOperations(name, version, "dev.obet.no", [
+      "publish",
+      "generate",
+      "deploy",
+    ]);
+    await runStageOperations(name, version, "stage.obet.no", [
+      "download",
+      "apply",
+      "deploy",
+    ]);
+    await runStageOperations(name, version, "test.obet.no", ["download"]);
+  } catch (error) {
+    console.log(
+      chalk.red(error.message),
+      error.lastSuccessfulStep &&
+        `(Last step completed: ${error.lastSuccessfulStep})`
+    );
+    process.exit(1);
+  }
 }
 
-publishFromDev().catch((error) => {
-  console.error(
-    error.message,
-    error.lastSuccessfulStep &&
-      `(Last step completed: ${error.lastSuccessfulStep})`
-  );
-  process.exit(1);
-});
+const cliPkg = await importJson("../package.json");
+let program = new Command();
+program
+  .version(cliPkg.version)
+  .argument(
+    "[package]",
+    "Specify name of the package to publish, otherwise the name in package.json is used"
+  )
+  .action(publishFromDev);
+
+await program.parseAsync();
