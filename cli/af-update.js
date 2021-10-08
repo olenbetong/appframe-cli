6bfccb724405bed699ea8ef18f38d4832677b7b6
Bjørnar Vister Hansen <bjornarvh@gmail.com>
2021-10-08 14:52:53 +0200

    Update both global and package

--- cli/af-update.js
+++ cli/af-update.js
@@ -5,15 +5,17 @@ try {
   let cliPkg = await importJson("../package.json");
   let args = ["install", "@olenbetong/appframe-cli@latest", "-g"];
 
+  console.log("npm", args.join(" "));
+  await spawnShellCommand("npm", args);
+
   try {
     let pkg = await importJson("./package.json", true);
     if (pkg.dependencies[cliPkg.name] || pkg.devDependencies[cliPkg.name]) {
       args.pop();
+      console.log("npm", args.join(" "));
+      await spawnShellCommand("npm", args);
     }
   } catch (error) {} // eslint-disable-line
-
-  console.log("npm", args.join(" "));
-  await spawnShellCommand("npm", args);
 
   console.log("Update completed.");
 } catch (error) {
