122b7830c5effa14245a31ae30681a8ac276c2d8
Bjørnar Vister Hansen <bvh@olenbetong.no>
2022-05-22 13:33:46 +0200

    Fix copy datasources for new config layout

--- cli/af-vite-copy-datasources.js
+++ cli/af-vite-copy-datasources.js
@@ -17,7 +17,7 @@ async function copyDatasourcesFromDev(articleWithHost) {
     try {
       const pkg = await importJson("./package.json", true);
       config = {
-        hostname: pkg.appframe.article?.id ?? pkg.appframe.hostname,
+        hostname: pkg.appframe.article?.hostname ?? pkg.appframe.hostname,
         article: pkg.appframe.article?.id ?? pkg.appframe.article,
       };
     } catch (error) {
