c7e7aa9353180beb7ef2feaae3652db40d97d0b2
Bjørnar Vister Hansen <bvh@olenbetong.no>
2022-08-08 13:20:17 +0200

    Try to fix self-updater on windows

--- src/af-update.ts
+++ src/af-update.ts
@@ -6,15 +6,24 @@ try {
   let cliPkg = await importJson("../package.json");
   let args = ["install", "@olenbetong/appframe-cli@latest", "-g"];
 
-  console.log("npm", args.join(" "));
-  await spawnShellCommand("npm", args);
+  console.log(process.platform === "win32" ? "npm.cmd" : "npm", args.join(" "));
+  await spawnShellCommand(
+    process.platform === "win32" ? "npm.cmd" : "npm",
+    args
+  );
 
   try {
     let pkg = await importJson("./package.json", true);
     if (pkg.dependencies?.[cliPkg.name] || pkg.devDependencies?.[cliPkg.name]) {
       args.pop();
-      console.log("npm", args.join(" "));
-      await spawnShellCommand("npm", args);
+      console.log(
+        process.platform === "win32" ? "npm.cmd" : "npm",
+        args.join(" ")
+      );
+      await spawnShellCommand(
+        process.platform === "win32" ? "npm.cmd" : "npm",
+        args
+      );
     } else {
       console.log(
         chalk.blue(
