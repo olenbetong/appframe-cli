9889d6d384a0b1879349ebffa051d2f957ad72b1
Bjørnar Vister Hansen <bvh@olenbetong.no>
2022-08-08 13:24:44 +0200

    DRY

--- src/af-update.ts
+++ src/af-update.ts
@@ -4,26 +4,18 @@ import { importJson } from "./lib/importJson.js";
 
 try {
   let cliPkg = await importJson("../package.json");
+  let cmd = process.platform === "win32" ? "npm.cmd" : "npm";
   let args = ["install", "@olenbetong/appframe-cli@latest", "-g"];
 
-  console.log(process.platform === "win32" ? "npm.cmd" : "npm", args.join(" "));
-  await spawnShellCommand(
-    process.platform === "win32" ? "npm.cmd" : "npm",
-    args
-  );
+  console.log(cmd, args.join(" "));
+  await spawnShellCommand(cmd, args);
 
   try {
     let pkg = await importJson("./package.json", true);
     if (pkg.dependencies?.[cliPkg.name] || pkg.devDependencies?.[cliPkg.name]) {
       args.pop();
-      console.log(
-        process.platform === "win32" ? "npm.cmd" : "npm",
-        args.join(" ")
-      );
-      await spawnShellCommand(
-        process.platform === "win32" ? "npm.cmd" : "npm",
-        args
-      );
+      console.log(cmd, args.join(" "));
+      await spawnShellCommand(cmd, args);
     } else {
       console.log(
         chalk.blue(
@@ -32,10 +24,7 @@ try {
       );
     }
   } catch (error: any) {
-    console.log(
-      chalk.blue("No package.json found, skipping local install"),
-      error.message
-    );
+    console.log(chalk.blue("No package.json found, skipping local install"));
   }
 
   console.log(chalk.green("Update completed."));
