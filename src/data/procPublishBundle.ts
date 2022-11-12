7682d115b8012b7147b73c1e9e76c7898ecade91
Bjørnar Vister Hansen <bjornarvh@gmail.com>
2022-11-12 13:04:51 +0100

    Replace article procedures with data resources

--- src/data/procPublishBundle.ts
+++ src/data/procPublishBundle.ts
@@ -1,8 +1,40 @@
-import { Client, Procedure } from "@olenbetong/appframe-data";
+import { Client, ProcedureAPI } from "@olenbetong/appframe-data";
+
+export type PublishBundleParams = {
+  project_id?: string;
+  ver_id?: string;
+  DESCription?: string;
+  issueid?: string;
+};
 
 export default (client: Client) =>
-  new Procedure({
+  new ProcedureAPI<PublishBundleParams, unknown>({
     client,
-    articleId: "af-bundle",
-    procedureId: "procBundleToProd",
+    procedureId: "sstp_Bundle_ToProduction",
+    parameters: [
+      {
+        name: "project_id",
+        type: "string",
+        hasDefault: false,
+        required: false,
+      },
+      {
+        name: "ver_id",
+        type: "string",
+        hasDefault: false,
+        required: false,
+      },
+      {
+        name: "DESCription",
+        type: "string",
+        hasDefault: false,
+        required: false,
+      },
+      {
+        name: "issueid",
+        type: "string",
+        hasDefault: false,
+        required: false,
+      },
+    ],
   });
--- src/data/procPublishWebAsset.ts
+++ src/data/procPublishWebAsset.ts
@@ -1,32 +1,45 @@
 import { Client, ProcedureAPI } from "@olenbetong/appframe-data";
 
+export type PublishWebAssetsParams = {
+  Hostname?: string;
+  Name?: string;
+  Type?: string;
+  Description?: string;
+  IssueID?: string;
+};
+
 export default (client: Client) =>
-  new ProcedureAPI<any, any>({
+  new ProcedureAPI<PublishWebAssetsParams, unknown>({
     client,
     procedureId: "sstp_WebSiteCMS_PublishWebAsset",
     parameters: [
       {
         name: "Hostname",
+        type: "string",
         hasDefault: false,
         required: false,
       },
       {
         name: "Name",
+        type: "string",
         hasDefault: false,
         required: false,
       },
       {
         name: "Type",
+        type: "string",
         hasDefault: false,
         required: false,
       },
       {
         name: "Description",
+        type: "string",
         hasDefault: false,
         required: false,
       },
       {
         name: "IssueID",
+        type: "string",
         hasDefault: false,
         required: false,
       },
