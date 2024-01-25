52a1d234353d299fbb83d13fdad8d517e1c5b1b8
Bjørnar Vister Hansen <bvh@olenbetong.no>
2024-01-25 14:13:07 +0100

    Add feature types when generating types

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,8 @@
 export { default as getArticlesDataObject } from "./dsArticles.js";
+export {
+	default as getArticlesFeaturesDataObject,
+	ArticlesFeaturesRecord,
+} from "./dsArticlesFeatures.js";
 export { default as getArticlesPermissionsDataObject } from "./dsArticlesPermissions.js";
 export { default as getArticlesVersionsDataObject } from "./dsArticlesVersions.js";
 export { default as getBundlesDataObject } from "./dsBundles.js";
